8cd1fc8492ef4b431f7b2c7810497b15abe02d4d
Mohammed Saad Abban <115649011+saadabban76@users.noreply.github.com>
2023-06-13 23:48:13 +0530

    Update Htblocks.js

--- src/components/Htblocks.js
+++ src/components/Htblocks.js
@@ -6,6 +6,7 @@ const Htblocks = ({ src, title, description }) => {
       className="w-full bg-[#1c243b] 
     p-10 shadow-md shadow-gray-800 sm:px-14"
     >
+    
       <div className="flex flex-col items-center gap-5 sm:flex-row sm:space-x-8">
         <img src={src} alt="wallet" className="" />
         <div className="flex flex-col gap-4 sm:space-x-4">
